a5ac3d5011ffda50c834a3beac6d5ac61df83b83
a25cho <a25cho@uwaterloo.ca>
2023-11-27 18:18:58 -0500

    add fields and methods to card

--- cards/card.h
+++ cards/card.h
@@ -1,13 +1,25 @@
 #include <string>
+#include "board.h"
 
 using namespace std;
 
+enum class CardType { Minion, Spell, Ritual, Enchantment };
+
 class Card {
-    string name;
+    string name, desc;
+    int cost;
 
     public:
-        Card(string name);
-        friend ostream &operator<<(ostream &out, Card c);
+        Card(string name, string desc, int cost);
 
+        virtual CardType getCardType() = 0;
+        virtual void notifyCardTurnStart();
+        virtual void notifyCardTurnEnd();
+        virtual void notifyCardMinionEnter();
+        virtual void notifyCardMinionLeave();
+        virtual void play(Board *board);
+        virtual void play(Board *board, Card *target);
+        int getCost();
 
+        friend ostream &operator<<(ostream &out, Card &c);
 };
