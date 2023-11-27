86b4ce952e7854decf183ee379f0c8e20b4bad0f
hx2he <hx2he@uwaterloo.ca>
2023-11-27 18:59:06 -0500

    defining player header

--- player.h
+++ player.h
@@ -1,20 +1,26 @@
 #ifndef __PLAYER_H__
-#define __PLAYER_H__
-#include <string>
+#define __PLAYER_H_
+#include <string>_
 #include <vector>
+#include <memory>
 
 using namespace std;
 
 class Player {
+    int id, magic;
     string name;
-    vector<string> deck;
+    vector<unique_ptr<int>> hand;
 
     public:
-        Player();
-        Player(string name, vector<string> deck);
-        void setName(string n); // remove later, once we create players w/ names
-        void setDeck(vector<string> deck); // same here
-
+        Player(int id, string name); // magic is initialized to 0 and hand is initially empty.
+        void startTurn();
+        void endTurn();
+        void drawCard();
+        void playCard();
+        void setMagic(const int n);
+        int getMagic() const;
+        int getID() const;
+        vector<unique_ptr<int>> getHand() const;
 };
 
 #endif
