6b92e88cd4540fd3f5f9897997ef1991787c0fcc
hx2he <hx2he@uwaterloo.ca>
2023-12-01 10:47:25 -0500

    adding method declarations

---
+++ printer.cc
@@ -0,0 +1,11 @@
+#include "printer.h"
+
+Printer::Printer() {}
+
+Printer::~Printer() {}
+
+void Printer::printBoard(vector<vector<unique_ptr<Card>>> board) {}
+
+void Printer::printHand(vector<unique_ptr<Card>> hand) {}
+
+void Printer::printInspect(unique_ptr<Minion> minion) {}
