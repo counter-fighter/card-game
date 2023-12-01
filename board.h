62552a4367d1ea8f5d254cb8d948efa4b07f1eaf
hx2he <hx2he@uwaterloo.ca>
2023-12-01 10:16:35 -0500

    adding board header methods and dtors

--- board.h
+++ board.h
@@ -2,15 +2,32 @@
 #define __BOARD_H__
 #include <string>
 #include <vector>
+#include <memory>
+#include "player.h"
+#include "card.h"
 
 using namespace std;
 
 class Board {
-      vector<>
+  vector<vector<unique_ptr<Card>>> board;
+  vector<unique_ptr<Player>> players;
+  // other fields (deck, graveyard, removedFromPlay?)
 
-    public:
-
-
+  public:
+    Board();
+    ~Board();
+    void damage(int n);
+    void playACard(unique_ptr<Card> card);
+    void summon(unique_ptr<Card> card, int n);
+    void attach(unique_ptr<Card> card);
+    void detach(unique_ptr<Card> card);
+    void notifyTurnStart();
+    void notifyTurnEnd();
+    void notifyMinionEnter();
+    void notifyMinionLeave();
+    Player getPlayer(int playerID);
+    vector<vector<unique_ptr<Card>>> getBoard();
+    vector<unique_ptr<Card>> getGraveyard(int playerID);
 };
 
 #endif
--- player.h
+++ player.h
@@ -14,6 +14,7 @@ class Player {
 
     public:
         Player(int id, string name); // magic is initialized to 0 and hand is initially empty.
+        ~Player();
         void startTurn();
         void endTurn();
         void drawCard();
