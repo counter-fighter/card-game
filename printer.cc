85a36e5f946af56dc14db5db6bfe700e814709a2
hx2he <hx2he@uwaterloo.ca>
2023-12-01 19:48:09 -0500

    implemented printer methods

--- printer.cc
+++ printer.cc
@@ -1,17 +1,88 @@
+#include <iostream>
 #include "printer.h"
+
+using namespace std;
 
 Printer::Printer() {}
 
 Printer::~Printer() {}
 
-void Printer::printBoard(vector<vector<unique_ptr<Card>>> board) {
+void Printer::printBoard(const Board& board) {
 
 }
 
-void Printer::printHand(vector<unique_ptr<Card>> hand) {
-
-}
-
-void Printer::printInspect(unique_ptr<Minion> minion) {
+void Printer::printHand(const vector<Card&> hand) {
     
+}
+
+void Printer::printInspect(Minion& minion) {
+    emplaceBackCard(minion);
+    printCards();
+    vector<Enchantment&> enchantments = minion.getEnchantments();
+    for (Enchantment& i:enchantments) {
+        emplaceBackCard(i);
+    }
+    printCards();
+}
+
+void Printer::printCards() {
+    int cardsPrinted = 0;
+    while (cardsPrinted < cards.size()) { // Prints 5 cards per row.
+        for (int line = 0; line < cardHeight; line++) {
+            for (int i = cardsPrinted; 
+                 i < cardsPrinted + maxCardPerRow && i < cards.size(); i++) {
+                cout << cards[i][line];
+            }
+            cout << endl;
+        } 
+        cardsPrinted += maxCardPerRow;
+    }
+    cards.clear();
+}
+
+void Printer::emplaceBackCard(Card& card) {
+    if (card.getCardType() == CardType::Minion) minionToCardTemplateT(static_cast<Minion&>(card));
+    else if (card.getCardType() == CardType::Ritual) {
+
+    } else if (card.getCardType() == CardType::Spell) {
+
+    } else {
+        enchantmentToCardTemplateT(static_cast<Enchantment&>(card));
+    }
+    cards.emplace_back(card);
+}
+
+void Printer::minionToCardTemplateT(const Minion& minion) {
+    if (minion.getDesc() == "") { // Minion with no ability.
+        card = display_minion_no_ability(minion.getName(), minion.getCost(), minion.getAttack(), 
+                                         minion.getDefence());
+    } else if (minion.getActivationCost() == 0) { // Minion with triggered ability.
+        card = display_minion_triggered_ability(minion.getName(), minion.getCost(), 
+                                                minion.getAttack(), minion.getDefence(), 
+                                                minion.getDesc());
+    } else { // Minion with activated ability.
+        card = display_minion_activated_ability(minion.getName(), minion.getCost(), 
+                                                minion.getAttack(), minion.getDefence(), 
+                                                minion.getActivationCost(), minion.getDesc());
+    }
+}
+
+void Printer::ritualToCardTemplateT(const Ritual& ritual) {
+    card = display_ritual(ritual.getName(), ritual.getCost(), ritual., ritual.getDesc(),
+                               int ritual_charges);
+}
+
+void Printer::spellToCardTemplateT(const Spell& spell) {
+    card = display_spell(spell.getName(), spell.getCost(), spell.getDesc());
+}
+
+void Printer::enchantmentToCardTemplateT(const Enchantment& enchantment) {
+    if (enchantment.getDesc() == "") { // Enchantment modifying attack/defense.
+        card = display_enchantment_attack_defence(enchantment.getName(), enchantment.getCost(),
+                                                  enchantment.getDesc(), enchantment.getAttack(),
+                                                  enchantment.getDefense());
+    } else { // Enchantment modifying ability.
+        card = display_enchantment(enchantment.getName(), enchantment.getCost(), 
+                                   enchantment.getDesc());
+    }
 }
--- printer.h
+++ printer.h
@@ -1,23 +1,34 @@
 #ifndef __PRINTER_H__
 #define __PRINTER_H__
 #include <vector>
-#include <memory>
+#include "board.h"
 #include "card.h"
 #include "minion.h"
+#include "ritual.h"
+#include "spell.h"
+#include "ascii_graphics.h"
 
 using namespace std;
 
 class Printer {
-    vector<vector<unique_ptr<Card>>> board;
-    vector<unique_ptr<Card>> hand;
-    unique_ptr<Minion> minion;
-     
+    const int cardHeight = 11;
+    const int maxCardPerRow = 5;
+    const card_template_t blank;
+    card_template_t card;
+    vector<card_template_t> cards;
+
     public:
         Printer();
         ~Printer();
-        void printBoard(vector<vector<unique_ptr<Card>>> board);
-        void printHand(vector<unique_ptr<Card>> hand);
-        void printInspect(unique_ptr<Minion> minion);
+        void printBoard(const Board& board);
+        void printHand(const vector<Card&> hand);
+        void printInspect(Minion& minion);
+        void printCards();
+        void emplaceBackCard(Card& card);
+        void minionToCardTemplateT(const Minion& minion);
+        void ritualToCardTemplateT(const Ritual& ritual);
+        void spellToCardTemplateT(const Spell& spell);
+        void enchantmentToCardTemplateT(const Enchantment& enchantment);
 };
 
 #endif
